8806b14f6bc7dadc48431364b329485f714fbf68
luqmanzakariya <48648183+luqmanzakariya@users.noreply.github.com>
2019-04-13 03:42:30 +0700

    Add files via upload

---
+++ [Exercises 7] Urutkan Abjad.js
@@ -0,0 +1,13 @@
+function urutkanAbjad(str) {
+    // you can only write your code here!
+    var arr =[];
+    arr=str.split('').sort().join('');
+    return arr;
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
